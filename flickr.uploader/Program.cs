e004c8bce559e69cfc29f927fa95bf9e47b33ce2
Pierre Gillon <gillon.pierre69@gmail.com>
2018-04-29 17:44:13 +1200

    Add CommandLineParser library.

--- flickr.uploader/Program.cs
+++ flickr.uploader/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommandLine;
 
 namespace flickr.uploader
 {
@@ -10,6 +7,28 @@ namespace flickr.uploader
     {
         static void Main(string[] args)
         {
+            Parser.Default.ParseArguments<Options>(args)
+                  .WithParsed(RunOptionsAndReturnExitCode);
         }
+
+        private static void RunOptionsAndReturnExitCode(Options options)
+        {
+            Console.WriteLine("done");
+        }
+    }
+
+    class Options
+    {
+        [Option(Required = true, HelpText = "The Flickr login")]
+        public string Login { get; set; }
+
+        [Option(Required = true, HelpText = "The Flick password")]
+        public string Password { get; set; }
+
+        [Option(Required = true, HelpText = "The folder to parse and to upload pictures")]
+        public string Folder { get; set; }
+
+        [Option(Required = true, HelpText = "The album name to upload the pictures.")]
+        public string Album { get; set; }
     }
 }
