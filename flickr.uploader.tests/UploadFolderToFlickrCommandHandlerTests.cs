1dc69c13acba777ffb7ec21f7964cc3ab0f03cf9
Pierre Gillon <gillon.pierre69@gmail.com>
2018-05-01 09:19:07 +1200

    Comment tests.

--- flickr.uploader.tests/UploadFolderToFlickrCommandHandlerTests.cs
+++ flickr.uploader.tests/UploadFolderToFlickrCommandHandlerTests.cs
@@ -4,19 +4,19 @@ namespace flickr.uploader.tests
 {
     public class UploadFolderToFlickrCommandHandlerTests
     {
-        [Fact]
-        public void test()
-        {
-            var command = new UploadFolderToFlickrCommand {
-                ApiKey = "a023233ad75a2e7ae38a1b1aa92ff751",
-                ApiSecret = "abd048b37b9e44f9",
-                PictureLocalFolder  = "Resources/",
-                PhotoSetId = "test"
-            };
+        //[Fact]
+        //public void test()
+        //{
+        //    var command = new UploadFolderToFlickrCommand {
+        //        ApiKey = "a023233ad75a2e7ae38a1b1aa92ff751",
+        //        ApiSecret = "abd048b37b9e44f9",
+        //        PictureLocalFolder  = "Resources/",
+        //        PhotoSetId = "test"
+        //    };
 
-            var handler = new UploadFolderToFlickrCommandHandler(new FlickrToken());
+        //    var handler = new UploadFolderToFlickrCommandHandler();
 
-            handler.Handle(command);
-        }
+        //    handler.Handle(command);
+        //}
     }
 }
